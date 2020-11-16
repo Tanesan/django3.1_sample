8deade55471df732a75f3ec93e00e6ccf75a3f10
j.takubo <j.takubo@datumstudio.jp>
2020-11-17 02:08:40 +0900

    add __str__ method

--- dev/home_monitoring/models.py
+++ dev/home_monitoring/models.py
@@ -5,4 +5,8 @@ class IoT(models.Model):
     datatype = models.CharField(max_length=20)
     pub_date = models.DateTimeField('date published')
     quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.datatype} at {self.pub_date}"
+    
     
