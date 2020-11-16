9c5b1ca02c78c88528b21f81bae9a199ae99a797
j.takubo <nyk523jt@gmail.com>
2020-11-17 03:32:20 +0900

    added custom login page

--- dev/webapp/settings.py
+++ dev/webapp/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -59,7 +58,7 @@ LOGOUT_REDIRECT_URL = "home"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,9 +110,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'ja'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
--- dev/webapp/urls.py
+++ dev/webapp/urls.py
@@ -20,5 +20,6 @@ from django.views.generic.base import TemplateView # new
 urlpatterns = [
     path('home_monitoring/', include('home_monitoring.urls')),
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home_monitoring/index.html'), name='home'), # new
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'), # new
 ]
