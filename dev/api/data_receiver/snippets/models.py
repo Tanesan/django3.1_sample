bb9a5a8339c8c2b8ffe2f7f798f9199db944399f
j.takubo <nyk523jt@gmail.com>
2020-11-17 06:01:21 +0900

    added api server

---
+++ dev/api/data_receiver/snippets/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class IOT(models.Model):
+    datatype = models.CharField(max_length=20)
+    pub_date = models.DateTimeField('date published')
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.datatype} at {self.pub_date}"
+    
+    
